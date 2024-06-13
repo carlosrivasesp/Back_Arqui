eb48b488ef8b60043153170f783b8c86820c66e6
Landeon0907 <npolitan22@gmail.com>
2024-06-13 15:20:39 -0500

    v1

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const conectarDB = require('./config/db');
-
+//crea servidor
 const app = express()
-
+//conectamos a la base de datos :)
 conectarDB();
-
-app.use(express.json())
+app.use('/api/productos',require('./router/producto'));
+//ruta principal
+//app.use(express.json())
 
 app.listen(4000, ()=>{
     console.log('El servidor está corriendo')
